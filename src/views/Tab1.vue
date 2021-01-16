11963f8fa57808ee266bd2954289db4370ff3a9e
JulienKEIFF <j.keiff@hotmail.fr>
2021-01-16 05:30:42 +0100

    Add(home):Add feed in home page

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -4,25 +4,66 @@
       <ion-toolbar>
         <ion-title>Accueil</ion-title>
       </ion-toolbar>
+      <ion-progress-bar type="indeterminate" v-if="loadingTime"></ion-progress-bar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Accueil</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <ExploreContainer name="Accueil" />
+      
+      <rssCard v-for="(item, i) in rssToDisplay" 
+        :key="i"
+        :title="item.title ? item.title[0] : 'Pas de Titre'" 
+        :category="item.category ? item.category : []" 
+        :description="item.description ? item.description[0] : 'Pas de description'"
+        :pubDate="item.pubDate ? item.pubDate[0] : ''"
+        from="test"
+      />
+
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonProgressBar } from '@ionic/vue';
+import rssCard from '../components/rss/rssCard'
 
 export default  {
   name: 'Accueil',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonProgressBar, rssCard },
+  data(){
+    return{
+      loadingTime: true,
+      rssToDisplay: [],
+      rssSrcList: []
+    }
+  },
+  methods: {
+    getDataFromSrc: function(){
+      this.loadingTime = true;
+      for (let i = 0; i < this.rssSrcList.length; i++) {
+        const link = this.rssSrcList[i];
+        if (link.rsslink !== "init"){
+          this.$http.get("https://cors-anywhere.herokuapp.com/" + link.rsslink)
+          .then(res=>{
+            this.$parseXML(res.data, (err, result)=>{
+              this.getFirstElements(result.rss.channel[0].item);
+            });
+          });
+        }
+        this.loadingTime = false;
+      }
+    },
+    getFirstElements: function(items){
+      for (let i = 0; i < 5; i++) {
+        const element = items[i];
+        this.rssToDisplay.push(element)
+      }
+    }
+  },
+  mounted: function(){
+    this.$dexie.transaction('r', this.$dexie.rssOff, async ()=>{
+      this.rssSrcList = await this.$dexie.rssOff.toArray();
+      this.rssSrcList = this.rssSrcList.slice(0)
+      this.getDataFromSrc();
+    });
+  }
 }
 </script>
